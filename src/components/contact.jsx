da8a4ebbaccc0f85bf41b810570f2ac267624f1d
Hector <141383937+hfelix3@users.noreply.github.com>
2024-01-28 14:55:28 -0700

    added email validator and required field message

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,15 +1,63 @@
+import { useState } from 'react';
+import emailValidator from 'email-validator';
+
 function ContactH(prop) {
-    return (
-      <div>
-        <h2 class ='contactForm'>Contact Me</h2>
-        <form class ='contactForm'>
-          <input placeholder="Enter Name"/>
-          <input placeholder="Enter E-mail"/>
-          <textarea id="textedTyped" placeholder="Write a message here" rows="5" cols="33"></textarea>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
+  const [NameValue, SetName] = useState('');
+
+  const [EmailValue, SetEmail] = useState('');
+  const [IsValid, SetIsValid] = useState(true);
+
+  const [MessageValue, SetMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (emailValidator.validate(EmailValue)) {
+      SetIsValid(true);
+    } else {
+      SetIsValid(false);
+    }
+  };
+
+  return (
+    <div>
+      <h2 class="contactForm">Contact Me</h2>
+      <form class="contactForm" onSubmit={handleSubmit}>
+        <input
+          name="Name"
+          value={NameValue}
+          onChange={(e) => SetName(e.target.value)}
+          onBlur={((e) => SetName(e.target.value))}
+          placeholder="Enter Name"
+          required
+        />
+
+        <input
+          name="Email"
+          value={EmailValue}
+          onChange={((e) => SetEmail(e.target.value))}
+          onBlur={(e) => SetEmail(e.target.value)}
+          placeholder="Enter E-mail"
+          required
+        />
+
+        <textarea
+          id="textedTyped"
+          placeholder="Write a message here"
+          rows="5"
+          cols="33"
+          name="Message"
+          value={MessageValue}
+          onChange={(e) => SetMessage(e.target.value)}
+          onBlur={((e) => SetMessage(e.target.value))}
+          required
+        ></textarea>
+
+        <button type="submit">Submit</button>
+        {!IsValid && <p>Please enter a valid email address.</p>}
+      </form>
+    </div>
+  );
 }
 
 export default ContactH;
